1211a39f8e8a8da353563c5d826deda9c21e79c2
Roma <r0509133009@gmail.com>
2019-05-15 09:52:47 +0300

    category discription done

--- views/catalog/category.php
+++ views/catalog/category.php
@@ -29,9 +29,13 @@
             <div class="col-sm-9 padding-right">
                 <div class="features_items"><!--features_items-->
                     <h2 class="title text-center">Последние товары</h2>
+                        <div class="panel-heading" style="text-align: center">
+                            <img src="<?php echo $categoryProducts[0]['category_image'];?>"/>
+                        </div>
+
                     <?php echo $categoryProducts[0]['category_description'];?>
                     <?php foreach ($categoryProducts as $product):?>
-                        <div class="col-sm-4">
+                <div class="col-sm-4">
                             <div class="product-image-wrapper">
                                 <div class="single-products">
                                     <div class="productinfo text-center">
